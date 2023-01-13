b3061fdb0398a3c9b1c23d9b9eeed867e8912c8c
phoenix <phoenix.worker@gmail.com>
2023-01-13 11:33:23 +0500

    Начальный коммит

---
+++ app/src/main/java/ru/phoenix/kidswatch/ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package ru.phoenix.kidswatch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()
---
+++ app/src/main/java/ru/phoenix/kidswatch/ui/fragments/main/MainFragment.kt
@@ -0,0 +1,5 @@
+package ru.phoenix.kidswatch.ui.fragments.main
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment()
