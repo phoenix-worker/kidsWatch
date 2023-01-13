db2e8d86ba4e5ea571e04a1d867aa7603fa90f6d
phoenix <phoenix.worker@gmail.com>
2023-01-13 11:51:23 +0500

    Базовая вёрстка

--- app/src/main/java/ru/phoenix/kidswatch/ui/MainActivity.kt
+++ app/src/main/java/ru/phoenix/kidswatch/ui/MainActivity.kt
@@ -1,5 +1,17 @@
 package ru.phoenix.kidswatch.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.phoenix.kidswatch.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
