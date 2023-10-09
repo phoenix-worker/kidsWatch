4e74bb748293d0e7e48bc2aec0431a001df033ce
phoenix <phoenix.worker@gmail.com>
2023-10-09 10:19:23 +0500

    Смена идентификатора AppMetrica

--- app/src/main/java/apps/cradle/kidswatch/App.kt
+++ app/src/main/java/apps/cradle/kidswatch/App.kt
@@ -32,7 +32,7 @@ class App : Application() {
     companion object {
 
         private lateinit var instance: App
-        private const val APP_METRICA_API_KEY = "d5b6819b-7e10-4988-b251-4d30506964cd"
+        private const val APP_METRICA_API_KEY = "03be3399-dce3-423e-9c60-8a635e537530"
         private val LOG_ENABLED = BuildConfig.DEBUG
         private const val DEBUG_TAG = "KIDS_WATCH_DEBUG"
 
